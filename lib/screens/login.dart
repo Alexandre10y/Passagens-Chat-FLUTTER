6103d874e45283a30ca48aea4805e017c9505082
alexandre10y <alexandre10y@outlook.com>
2023-12-05 18:36:06 -0300

    upload

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -4,9 +4,15 @@ import 'package:provinha_god/screens/registro.dart';
 
 import '../widgets/login_bar.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,88 +26,109 @@ class Login extends StatelessWidget {
           ],
         ),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("images/city2.jpg"),
-            fit: BoxFit.cover,
-            opacity: 0.7,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/city2.jpg"),
+                fit: BoxFit.cover,
+                opacity: 0.7,
+              ),
+            ),
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  labelText: 'E-mail',
-                  fillColor: Colors.white,
-                  filled: true,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: 'E-mail',
+                    fillColor: Colors.white,
+                    filled: true,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: TextField(
-                controller: passwordController,
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  fillColor: Colors.white,
-                  filled: true,
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TextField(
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    fillColor: Colors.white,
+                    filled: true,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Home()),
-                );
-              },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 115, 191, 250)),
-                padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 40, vertical: 20)),
-                shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                )),
-                overlayColor: MaterialStateProperty.all(Color.fromARGB(255, 249, 250, 252).withOpacity(0.7)),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isLoading = true;
+                  });
+
+                  // Simula um processo assíncrono (pode ser uma chamada de API, autenticação, etc.)
+                  Future.delayed(Duration(seconds: 2), () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Home()),
+                    );
+                    setState(() {
+                      isLoading = false;
+                    });
+                  });
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 115, 191, 250)),
+                  padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 40, vertical: 20)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  )),
+                  overlayColor: MaterialStateProperty.all(Color.fromARGB(255, 249, 250, 252).withOpacity(0.7)),
+                ),
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontSize: 18),
+                ),
               ),
-              child: Text(
-                'Login',
-                style: TextStyle(fontSize: 18),
+              SizedBox(height: 30),
+              Text(
+                'Ainda não possui uma conta? Crie agora!',
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Registro()),
+                  );
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 164, 218, 191)),
+                  padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 40, vertical: 20)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  )),
+                  overlayColor: MaterialStateProperty.all(Color.fromARGB(255, 249, 250, 249).withOpacity(0.7)),
+                ),
+                child: Text(
+                  'Registrar',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
+          if (isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.5),
+              child: Center(
+                child: CircularProgressIndicator(),
               ),
             ),
-            SizedBox(height: 30),
-            Text(
-              'Ainda não possui uma conta? Crie agora!',
-              style: TextStyle(fontSize: 16, color: Colors.white),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Registro()),
-                );
-              },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 164, 218, 191)),
-                padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 40, vertical: 20)),
-                shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                )),
-                overlayColor: MaterialStateProperty.all(Color.fromARGB(255, 249, 250, 249).withOpacity(0.7)),
-              ),
-              child: Text(
-                'Registrar',
-                style: TextStyle(fontSize: 18),
-              ),
-            ),
-          ],
-        ),
+        ],
       ),
     );
   }
